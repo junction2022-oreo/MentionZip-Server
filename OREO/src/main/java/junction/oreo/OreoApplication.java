a84eb8a7eba410379148fd6037a3860d191dcc08
goodGid <hello.goodgid@gmail.com>
2022-08-19 19:30:08 +0900

    Init project setup

---
+++ OREO/src/main/java/junction/oreo/OreoApplication.java
@@ -0,0 +1,13 @@
+package junction.oreo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OreoApplication.class, args);
+    }
+
+}
---
+++ OREO/src/test/java/junction/oreo/OreoApplicationTests.java
@@ -0,0 +1,13 @@
+package junction.oreo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OreoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
