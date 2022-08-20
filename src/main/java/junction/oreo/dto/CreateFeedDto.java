45718f961f3ce705dd5aae68be7493db229495bc
goodGid <hello.goodgid@gmail.com>
2022-08-20 17:50:11 +0900

    Add `CreateFeedDto`

---
+++ src/main/java/junction/oreo/dto/CreateFeedDto.java
@@ -0,0 +1,15 @@
+package junction.oreo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateFeedDto {
+    private String mid;
+    private String text;
+}
