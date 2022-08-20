48025aa10ce180e4f255f7d56c4578effd3a4f46
goodGid <hello.goodgid@gmail.com>
2022-08-20 17:48:37 +0900

    Implement common setting

---
+++ src/main/java/junction/oreo/exception/CustomException.java
@@ -0,0 +1,22 @@
+
+package junction.oreo.exception;
+
+import junction.oreo.enums.response.CodeEnum;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CustomException extends RuntimeException {
+    private String returnCode;
+    private String returnMessage;
+
+    public CustomException(CodeEnum codeEnum) {
+        super(codeEnum.getMessage());
+        setReturnCode(codeEnum.getCode());
+        setReturnMessage(codeEnum.getMessage());
+    }
+
+}
