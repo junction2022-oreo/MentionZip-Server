9e6fa8d0c82599dff0736be45c6866b40591b93c
goodGid <hello.goodgid@gmail.com>
2022-08-20 17:50:28 +0900

    Add `MemberDto` class

---
+++ src/main/java/junction/oreo/dto/MemberDto.java
@@ -0,0 +1,18 @@
+package junction.oreo.dto;
+
+import junction.oreo.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDto {
+
+    private Member member;
+}
